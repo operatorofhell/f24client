075ca8874d6216ed2138ba5be63e397d45b49082
Moe <moritz.kraus@muellergroup.com>
2014-11-16 17:31:26 +0000

    new cli interface

---
+++ f24alert.php
@@ -0,0 +1,43 @@
+<?php
+
+require("lib/F24Client.php");
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+$options = getopt("u:p:i:dhm:");
+
+if ( isset($options['h']) ){
+
+	echo "Usage:	f24client [-d|-h] -u<username> -p<password> -i<alert id> -m\"<message>\"";
+	echo "\n";
+
+	exit(1);
+}
+
+if ( ! isset($options['u']) ){
+	echo "username is missing\n";
+	exit(2);
+}
+
+if ( ! isset($options['p']) ){
+	echo "password is missing\n";
+	exit(2);
+}
+
+if ( ! isset($options['i']) ){
+	echo "alert id is missing\n";
+	exit(2);
+}
+
+if ( isset($options['d']) ){
+	error_reporting(E_ALL);	
+}
+	
+
+
+$f24 = new F24Client($options['u'], $options['p']);
+
+$f24->sendAlarm( $options['i'], $options['m']);
+
+
+?>
